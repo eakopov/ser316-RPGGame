95ed939d1b83b588ec896785c510e5be57a58c43
Elizabeth Akopov <156018147+eakopov@users.noreply.github.com>
2024-02-14 16:37:06 -0600

    Created Character and Factory classes

---
+++ src/main/java/CharacterFactory.java
@@ -0,0 +1,4 @@
+public abstract class CharacterFactory {
+    // Factory method for creating characters
+    public abstract Character createCharacter(String sex, String hairColor, String hairLength, String eyeColor, String height, String weight);
+}
