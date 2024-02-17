4df67ab783545f44d2769c4bc4eba08adc6c0dff
Elizabeth Akopov <156018147+eakopov@users.noreply.github.com>
2024-02-17 15:52:34 -0600

    Completed race classes

--- src/main/java/characters/Orc.java
+++ src/main/java/characters/Orc.java
@@ -1,4 +1,15 @@
 package characters;
 
-public class Orc {
+public class Orc extends Character {
+    public Orc(String name, String sex, String hairColor, String hairLength, String eyeColor, String height, String weight, String charClass) {
+        super(name, sex, hairColor, hairLength, eyeColor, height, weight, charClass, "Orc");
+    }
+
+    // Initialize speed and skill based on character race
+    @Override
+    public void initializeStats() {
+        this.speed = 5;
+        this.skill = "+5 starting defense";
+    }
+
 }
