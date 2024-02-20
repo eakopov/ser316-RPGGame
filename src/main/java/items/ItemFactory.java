d6c611adff434d2a00e06ed55256081b7cf64a3a
Elizabeth Akopov <156018147+eakopov@users.noreply.github.com>
2024-02-20 17:20:06 -0600

    Update ItemFactory.java

--- src/main/java/items/ItemFactory.java
+++ src/main/java/items/ItemFactory.java
@@ -3,9 +3,15 @@ package items;
 public class ItemFactory {
     public Item createItem(String itemName, String itemCondition) {
         switch (itemName.toLowerCase()) {
-            case "helmet", "boots", "shield":
+            case "helmet":
                 return new ArmorFactory().createArmor(itemName, itemCondition);
-            case "health", "mana":
+            case "boots":
+                return new ArmorFactory().createArmor(itemName, itemCondition);
+            case "shield":
+                return new ArmorFactory().createArmor(itemName, itemCondition);
+            case "health":
+                return new Potion(itemName, itemCondition);
+            case "mana":
                 return new Potion(itemName, itemCondition);
             default:
                 throw new IllegalArgumentException("Invalid item type: " + itemName);
